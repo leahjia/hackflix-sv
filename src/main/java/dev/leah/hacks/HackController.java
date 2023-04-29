a35b8124003eb8c31c02884dc486046e46b8780b
Leah Jia <yuwenj3@uw.edu>
2023-04-29 15:56:17 -0700

    entry

--- src/main/java/dev/leah/hacks/HackController.java
+++ src/main/java/dev/leah/hacks/HackController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +15,13 @@ public class HackController {
   @Autowired
   private HackService hackService;
   
+  @CrossOrigin
   @GetMapping
   public ResponseEntity<List<Hack>> getAllHacks() {
     return new ResponseEntity<>(hackService.allHacks(), HttpStatus.OK);
   }
   
+  @CrossOrigin
   @GetMapping("/{hackId}")
   public ResponseEntity<Optional<Hack>> getSingleHack(@PathVariable String hackId) {
     return new ResponseEntity<>(hackService.singleHack(hackId), HttpStatus.OK);
